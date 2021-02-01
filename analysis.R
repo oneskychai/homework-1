5015c80f26784c92be37028d885db5b0ac710454
Christopher Carbone <oneskychai@gmail.com>
2021-02-01 11:44:03 -0800

    Remove comments

--- analysis.R
+++ analysis.R
@@ -1,12 +1,9 @@
-# Load library and data
 library(tidyverse)
 load("rdas/murders.rda")
 
-# Make plot
 murders %>% mutate(abb = reorder(abb, rate)) %>%
   ggplot(aes(abb, rate)) +
   geom_bar(width = 0.5, stat = "identity", color = "black") +
   coord_flip()
-
-# Save plot  
+  
 ggsave("figs/barplot.png")
